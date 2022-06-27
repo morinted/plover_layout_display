09bbb4f980f4020d68e7bbf19da2a8a5d82045e5
Benoit Pierre <benoit.pierre@gmail.com>
2022-06-28 00:45:09 +0200

    setup: fix editable installation

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 
 from setuptools import setup
 
-from plover_build_utils.setup import BuildPy, BuildUi
+from plover_build_utils.setup import BuildPy, BuildUi, Develop
 
 BuildPy.build_dependencies.append('build_ui')
 BuildUi.hooks = ['plover_build_utils.pyqt:fix_icons']
+Develop.build_dependencies.append('build_py')
 cmdclass = {
     'build_py': BuildPy,
     'build_ui': BuildUi,
+    'develop': Develop,
 }
 
 setup(cmdclass=cmdclass)
